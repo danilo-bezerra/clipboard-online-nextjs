7619eff1b186746939fd7d27ee7efec52b810f51
Danilo Bezerra <danilobezerra970@gmail.com>
2023-12-17 22:34:52 -0300

    add footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,10 +19,27 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br">
-      <body className={cn("dark  dark:bg-slate-800", inter.className)}>
+      <body
+        className={cn(
+          "dark  dark:bg-slate-800 h-full flex flex-col",
+          inter.className
+        )}
+      >
         <ToastProvider />
         <Header />
         {children}
+        <footer className="p-6 dark:bg-slate-800">
+          <p className="text-center text-sm ">
+            Criado por{" "}
+            <a
+              className="hover:underline transition-all"
+              href="https://github.com/danilo-bezerra"
+              target="_blank"
+            >
+              <strong>Danilo Bezerra</strong>
+            </a>
+          </p>
+        </footer>
       </body>
     </html>
   );
